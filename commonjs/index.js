49ae990c87112592a642a44c2013641b8a3aeb83
Victor Cottin <victor@cottin.se>
2021-10-19 20:45:53 +0200

    initial commit

---
+++ commonjs/index.js
@@ -0,0 +1,4 @@
+var timeHelper = require('date-and-time-helpers2')
+
+console.log(timeHelper({startDate: '2021-08-01', endDate: '2021-08-31'}))
+console.log(timeHelper({startDate: '2021-01-01', endDate: '2021-12-31'}))
